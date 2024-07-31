03260222745536cd80e9ef79135c60b930118228
m1m0zzz <contact.m1m0zzz@gmail.com>
2024-07-31 17:07:34 +0900

    add: demo

--- README.md
+++ README.md
@@ -11,7 +11,13 @@ Convert JUCE code to Web Audio API AudioNode. (🧪Experimental implementation)
 
 ## Roadmap
 
-- [ ] Use AudioWorklet and WebAssembly to enable C++ DSP code to run in the browser. (on `/demo`)  
+- [x] Use AudioWorklet and WebAssembly to enable C++ DSP code to run in the browser. (on `/demo`)  
 AudioWorkletとWebAssemblyを使用して、C++のDSPコードをブラウザで実行できるようにする。(`/demo`)
 - [ ] Output the code of the test plugin (`/plugin`) as an AudioNode class or a single web page.  
   テストプラグイン(`/plugin`)のコードをAudioNodeクラスまたは単一のウェブページとして出力する。
+
+## Acknowledgments
+The following code is used in this project.  
+Thank you very much!  
+
+- [GoogleChromeLabs/web-audio-samples](https://github.com/GoogleChromeLabs/web-audio-samples)
--- demo/src/App.tsx
+++ demo/src/App.tsx
@@ -1,34 +1,54 @@
-import { useState } from 'react'
-import reactLogo from './assets/react.svg'
-import viteLogo from '/vite.svg'
+import { useEffect, useState } from 'react'
 import './App.css'
 
 function App() {
-  const [count, setCount] = useState(0)
+  const [isLoad, setIsLoad] = useState(false)
+  const [audioContext, setAudioContext] = useState<AudioContext | null>(null)
+
+  const startAudio = async (context: AudioContext) => {
+    console.log("startAudio")
+    await context.audioWorklet.addModule('/processors/wasm-worklet-processor.js');
+    const oscillator = new OscillatorNode(context);
+    const bypasser = new AudioWorkletNode(context, 'wasm-worklet-processor');
+    oscillator.connect(bypasser).connect(context.destination);
+    oscillator.start();
+  };
+
+  const loadAudioContext = () => {
+    let ctx: AudioContext
+    // if (!audioContext) {
+    // }
+    ctx = new AudioContext()
+    setAudioContext(ctx)
+    console.log("Loaded: AudioContext")
+    return ctx;
+  }
+
+  useEffect(() => {
+    setIsLoad(true)
+  })
 
   return (
-    <>
-      <div>
-        <a href="https://vitejs.dev" target="_blank">
-          <img src={viteLogo} className="logo" alt="Vite logo" />
+    <main>
+      <h1>AudioWorklet + WebAssembly</h1>
+      <p>GitHub:{" "}
+        <a href="https://github.com/m1m0zzz/juce-web-audio-builder/tree/main/demo" target="_blank" rel="noopener noreferrer">
+          m1m0zzz/juce-web-audio-builder/demo
         </a>
-        <a href="https://react.dev" target="_blank">
-          <img src={reactLogo} className="logo react" alt="React logo" />
-        </a>
-      </div>
-      <h1>Vite + React</h1>
-      <div className="card">
-        <button onClick={() => setCount((count) => count + 1)}>
-          count is {count}
-        </button>
-        <p>
-          Edit <code>src/App.tsx</code> and save to test HMR
-        </p>
-      </div>
-      <p className="read-the-docs">
-        Click on the Vite and React logos to learn more
       </p>
-    </>
+      <button
+        onClick={async () => {
+          let ctx: AudioContext
+          if (!audioContext) {
+            ctx = loadAudioContext();
+          } else {
+            ctx = audioContext
+          }
+          await startAudio(ctx);
+          ctx.resume();
+        }}
+      >start audio</button>
+    </main>
   )
 }
 
